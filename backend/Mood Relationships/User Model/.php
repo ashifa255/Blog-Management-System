cb347bdb1c620d41c745a1fc4c705f49da7bb65e
Ashifa Tamanna <astammannahifa0330@gmail.com>
2025-12-29 11:17:22 +0600

    Update .php

--- backend/Mood Relationships/User Model/.php
+++ backend/Mood Relationships/User Model/.php
@@ -1,1 +1,25 @@
-
+// app/Models/User.php
+class User extends Authenticatable
+{
+    use HasApiTokens;
+    
+    public function posts()
+    {
+        return $this->hasMany(Post::class);
+    }
+    
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
+    
+    public function likes()
+    {
+        return $this->hasMany(Like::class);
+    }
+    
+    public function bookmarks()
+    {
+        return $this->hasMany(Bookmark::class);
+    }
+}
